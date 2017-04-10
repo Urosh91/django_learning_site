4090501685e244df104549238c95ac1d431905fe
Uros Jevremovic <urosh43@gmail.com>
2017-04-10 14:21:27 +0200

    init commit

--- learning_site/views.py
+++ learning_site/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from . import forms
+
 
 def home(request):
     return render(request, 'home.html')
+
+
+def suggestion_view(request):
+    form = forms.SuggestionForm()
+    return render(request, 'suggestion_form.html', {'form': form})
