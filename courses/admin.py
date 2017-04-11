3bd6c159325b2ca68a08974aea08f888aeb3e129
Uros Jevremovic <urosh43@gmail.com>
2017-04-11 15:34:02 +0200

    init commit

--- courses/admin.py
+++ courses/admin.py
@@ -3,15 +3,24 @@ from django.contrib import admin
 from . import models
 
 
-class StepInLine(admin.StackedInline):
-    model = models.Step
-    # This creates an inline, which we can then use
+# class TextInLine(admin.StackedInline):
+#     model = models.Text
+#     # This creates an inline, which we can then use
+#
+#
+# class QuizInLine(admin.StackedInline):
+#     model = models.Quiz
+#
+#
+# class CourseAdmin(admin.ModelAdmin):
+#     inlines = [TextInLine, QuizInLine]
 
 
-class CourseAdmin(admin.ModelAdmin):
-    inlines = [StepInLine,]
-
-
-admin.site.register(models.Course, CourseAdmin)
+admin.site.register(models.Course)
 # Register Course as CourseAdmin
-admin.site.register(models.Step)
+admin.site.register(models.Text)
+admin.site.register(models.Quiz)
+admin.site.register(models.MultipleChoiceQuestion)
+admin.site.register(models.TrueFalseQuestion)
+admin.site.register(models.Answer)
+
--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from itertools import chain
 
 from . import models
 
@@ -15,10 +16,17 @@ def course_detail(request, pk):
     course = get_object_or_404(models.Course, pk=pk)
     # pk is ID by default
     # request comes in automatically and pk needs to be provided
-    return render(request, 'courses/course_detail.html', {'course': course})
+    steps = sorted(chain(course.text_set.all(), course.quiz_set.all()),
+                   key = lambda step: step.order)
+    return render(request, 'courses/course_detail.html', {'course': course, 'steps': steps})
 
 
-def step_detail(request, course_pk, step_pk):
-    step = get_object_or_404(models.Step, course_id = course_pk, pk = step_pk)
-    return render(request, 'courses/step_detail.html', {'step': step})
+def text_detail(request, course_pk, step_pk):
+    step = get_object_or_404(models.Text, course_id = course_pk, pk = step_pk)
+    return render(request, 'courses/text_detail.html', {'step': step})
+
+
+def quiz_detail(request, course_pk, step_pk):
+    step = get_object_or_404(models.Quiz, course_id = course_pk, pk = step_pk)
+    return render(request, 'courses/quiz_detail.html', {'step': step})
 
