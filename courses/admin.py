6e76ef51edc465538770e27c9f20add411eb1dd1
Uros Jevremovic <urosh43@gmail.com>
2017-04-16 21:53:02 +0200

    init commit

--- courses/admin.py
+++ courses/admin.py
@@ -49,15 +49,21 @@ class CourseAdmin(admin.ModelAdmin):
 
     list_filter = ['created_at', 'teacher', YearListFilter,]
 
+    list_display = ['title', 'created_at']
+
 
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [AnswerInLine,]
 
     search_fields = ['prompt']
 
+    list_display = ['prompt', 'quiz']
+
 
 class QuizAdmin(admin.ModelAdmin):
     fields = ['course', 'title', 'description', 'order', 'total_questions']
+
+    list_display = ['title', 'total_questions']
 
 admin.site.register(models.Course, CourseAdmin)
 # Register Course as CourseAdmin
